28b096061d8d6c3fde524ddf248f6843d6e899e9
Tekendra Bhandari <tekendraa67@gmail.com>
2026-05-17 12:43:03 +0545

    vzxv

--- Day02/data_type.py
+++ Day02/data_type.py
@@ -3,8 +3,12 @@ b = 3.14
 c = 5j
 d = "Hello, World!"
 e = 'Python is great!'
+bool_var = True
+f = False
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
 print(type(e))
+print(type(bool_var))
+print(type(f))
