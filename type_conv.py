e80a2cca10c21b5e47a7d28fc47c5e1d7b65b496
Tekendra Bhandari <tekendraa67@gmail.com>
2026-05-17 13:12:31 +0545

    boolean

--- type_conv.py
+++ type_conv.py
@@ -3,3 +3,8 @@ print(type(a)) # checking the type of a
 a = str(a) # converting integer to string
 print(a)
 print(type(a)) # checking the type of a
+b = 0
+print(type(b)) # checking the type of b
+b = bool(b) # converting integer to boolean
+print(b)
+print(type(b)) # checking the type of b
