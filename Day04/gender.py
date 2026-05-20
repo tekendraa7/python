c8f28ee412647bf662747b27dbd3e4570faed4da
Tekendra Bhandari <tekendraa67@gmail.com>
2026-05-20 19:40:26 +0545

    gender

---
+++ Day04/gender.py
@@ -0,0 +1,10 @@
+gender = input("Enter the gender M/F : ")
+
+if gender == 'M' or gender == 'm' :
+   print("Good Morning Sir!")
+   print("!")
+elif gender == 'F' or gender == 'f' :
+   print("Good morning ma'am!")
+
+else :
+   print("Invalid input please enter M/F : ")
