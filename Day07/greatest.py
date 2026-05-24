9ed8d52764b0667adc188ba998fea137a82025b7
Tekendra Bhandari <tekendraa67@gmail.com>
2026-05-24 15:30:20 +0545

    sd

--- Day07/greatest.py
+++ Day07/greatest.py
@@ -1,1 +1,9 @@
 l = [12, 3, 4, 6, 19, 30, 45, 7]
+largest = l[0]
+index = 0
+for i in range(len(l)):
+    print(l[i])
+    if l[i] > largest:
+        largest = l[i]
+        index = i
+print(f"The largest number is {largest} at index {index}. ")
