76bbd11614daf65b46b5053609c5dd01493d625f
Tekendra Bhandari <tekendraa67@gmail.com>
2026-05-24 14:31:28 +0545

    list

--- Day07/lists.py
+++ Day07/lists.py
@@ -1,2 +1,7 @@
 numbers = [1,2,3,4,5,6,7,8,9]
-numbers.append (10)
+numbers.append (10) #adds 10 to the end
+print(numbers)
+numbers.insert(2, 12) # insert 12 in index 2
+print(numbers)
+numbers.sort()
+print(numbers)
