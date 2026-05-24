d39c1a1c0c6626a02a19cffb327fe6f487c32d87
Tekendra Bhandari <tekendraa67@gmail.com>
2026-05-24 15:20:32 +0545

    ssff

---
+++ Day07/q1.py
@@ -0,0 +1,10 @@
+l = [-1, 2, -3, 4, -5, 6, -7, 8, -9]
+print("positive ar :")
+for i in l:
+    if i >= 0:
+        print(i)
+print('Negative are')
+for i in l:
+   if i <=0:
+       print(i)
+      
